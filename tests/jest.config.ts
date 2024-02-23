81fcc376df4914accdee33232a8ff20fa7b5e020
LaomaNogueira <laomanogueira@gmail.com>
2024-02-23 02:27:09 -0300

    test: add unit test to mapToSnakeCase helper

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -1,25 +1,21 @@
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '..',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   cache: false,
   clearMocks: true,
   testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': 'ts-jest'
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^src/(.*)$': '<rootDir>/src/$1',
     '^tests/(.*)$': '<rootDir>/tests/$1',
     '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@tests/(.*)$': '<rootDir>/tests/$1',
+    '^@tests/(.*)$': '<rootDir>/tests/$1'
   },
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!**/node_modules/**',
-    '!**/*dto.ts',
-    '!**/*enum.ts',
-  ],
-  coverageDirectory: './coverage',
+  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**', '!**/*dto.ts', '!**/*enum.ts'],
+  coverageDirectory: './coverage'
 };
