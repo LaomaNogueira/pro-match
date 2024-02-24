ed6b4624a617abd539032e452f07695736ab226c
LaomaNogueira <laomanogueira@gmail.com>
2024-02-24 00:58:40 -0300

    style: format code

--- src/infrastructure/services/http/axios/axios-client.service.ts
+++ src/infrastructure/services/http/axios/axios-client.service.ts
@@ -2,41 +2,23 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { IHttpService } from '@/domain/services-interfaces/http/http-service.interface';
 
 export class AxiosClientService implements IHttpService<any, any, any> {
-  public async get<T>(
-    url: string,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
+  public async get<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return axios.get<T>(url, config);
   }
 
-  public async patch<T>(
-    url: string,
-    data: any,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
+  public async patch<T>(url: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return axios.patch<T>(url, data, config);
   }
 
-  public async put<T>(
-    url: string,
-    data: any,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
+  public async put<T>(url: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return axios.put<T>(url, data, config);
   }
 
-  public async post<T>(
-    url: string,
-    data: any,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
+  public async post<T>(url: string, data: any, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return axios.post<T>(url, data, config);
   }
 
-  public async delete<T>(
-    url: string,
-    config?: AxiosRequestConfig,
-  ): Promise<AxiosResponse<T>> {
+  public async delete<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
     return axios.delete<T>(url, config);
   }
 }
