833bfd963ba8554e10bb0540070f5e45e06644d2
LaomaNogueira <laomanogueira@gmail.com>
2024-02-26 12:51:13 -0300

    style: remove whitespace

--- src/main/config/setup-middlewares.ts
+++ src/main/config/setup-middlewares.ts
@@ -4,7 +4,6 @@ import { cors } from '@/main/middlewares/cors';
 import { contentType } from '@/main/middlewares/content-type';
 import { xPoweredBy } from '@/main/middlewares/x-powered-by';
 
-
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser);
   app.use(cors());
