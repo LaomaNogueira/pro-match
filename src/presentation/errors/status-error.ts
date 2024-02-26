41a89f30087e7296471a7a7dbb0dc157096e6177
LaomaNogueira <laomanogueira@gmail.com>
2024-02-26 12:55:54 -0300

    feat: insert string type in error param

--- src/presentation/errors/status-error.ts
+++ src/presentation/errors/status-error.ts
@@ -1,14 +1,17 @@
 class StatusError extends Error {
-    status;
+  status;
 
-    constructor(status: number, error: Error) {
-        super();
-        this.status = status;
-        this.name = 'StatusError';
-        this.message = error.message
-        ? error.message
-        : 'Check the error code in the documentation';
+  constructor(status: number, error: Error | string) {
+    super();
+    this.status = status;
+    this.name = 'StatusError';
+    
+    if (error instanceof Error) {
+      this.message = error.message ? error.message : 'Check the error code in the documentation';
+    } else {
+      this.message = error;
     }
+  }
 }
 
 export { StatusError };
