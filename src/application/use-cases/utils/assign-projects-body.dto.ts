088047c8bc8f7697c0254c228ac3dd5280219c14
LaomaNogueira <laomanogueira@gmail.com>
2024-02-26 16:24:04 -0300

    feat: insert validation of required fields in AssignProjectsBodyDto

--- src/application/use-cases/utils/assign-projects-body.dto.ts
+++ src/application/use-cases/utils/assign-projects-body.dto.ts
@@ -1,41 +1,51 @@
 import { Type } from 'class-transformer';
-import { IsBoolean, IsEnum, IsNumber, IsString, ValidateNested } from 'class-validator';
+import { IsBoolean, IsEnum, IsNotEmpty, IsNotEmptyObject, IsNumber, IsString, ValidateNested } from 'class-validator';
 import { EducationLevel } from '@/application/use-cases/utils/education-level.enum';
 
 class PastExperiences {
+  @IsNotEmpty()
   @IsBoolean()
   sales: boolean;
 
+  @IsNotEmpty()
   @IsBoolean()
   support: boolean;
 }
 
 class InternetTest {
+  @IsNotEmpty()
   @IsNumber()
   download_speed: number;
 
+  @IsNotEmpty()
   @IsNumber()
   upload_speed: number;
 }
 
 export class AssignProjectsBodyDto {
+  @IsNotEmpty()
   @IsNumber()
   age: number;
 
+  @IsNotEmpty()
   @IsEnum(EducationLevel)
   education_level: EducationLevel;
 
+  @IsNotEmptyObject()
   @ValidateNested()
   @Type(() => PastExperiences)
   past_experiences: PastExperiences;
 
+  @IsNotEmptyObject()
   @ValidateNested()
   @Type(() => InternetTest)
   internet_test: InternetTest;
 
+  @IsNotEmpty()
   @IsNumber()
   writing_score: number;
 
+  @IsNotEmpty()
   @IsString()
   referral_code?: string;
 }
